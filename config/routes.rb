017aa0d89e6998eb9a1cef04783aef1cf4dead1e
Diego A Gomez <diagomezpa@unal.edu.co>
2017-02-10 21:21:30 -0500

    route to contact us

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   get 'pages#index'
 
-  
+
+  get 'contactUS'
+ to:   'pages#contactUS'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
