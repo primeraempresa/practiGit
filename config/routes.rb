6443eca48f19858fedb7a35942ca2aacdd4191ee
Diana A Gomez <dieosiris23@hotmail.com>
2017-02-10 21:42:18 -0500

    route to aboutUS

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   get 'pages#index'
 
-  
+
+  get 'AboutUS'
+ to:   'pages#aboutUS'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
